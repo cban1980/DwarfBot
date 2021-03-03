504163216395e822df4af52956445491047999a2
zphinx <cban@gmx.com>
2021-03-03 21:11:46 +0100

    add token settings

--- dwarfbot.py
+++ dwarfbot.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 include discord
 from discord.ext import commands
+# External settings from dot dir
+# Yes MR Polnäs, you need an external token directory
+HOMEDIR = os.path.expanduser('~')
+TOKENHOME = "%s/.DwarfSettings/" % (HOMEDIR)
+with open(TOKENHOME + "token.txt", "r") as readfile:
+    TOKEN = readfile.read().strip()
 
 bot = commands.Bot(command_prefix='!')
+ot.run(TOKEN)
