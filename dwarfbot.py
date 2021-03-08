f1339b83bbfb879ece20cfcaf48d1a7793f89ff8
zphinx <cban@gmx.com>
2021-03-08 18:43:00 +0100

    add more libraries

--- dwarfbot.py
+++ dwarfbot.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 import discord
 from discord.ext import commands
+from discord.ext.commands import Bot
 import os
+import requests
+from bs4 import BeautifulSoup as bs
+
 # External settings from dot dir
 # Yes Mr Polnäs, you need an external token directory
 HOMEDIR = os.path.expanduser('~')
 TOKENHOME = "%s/.DwarfSettings/" % (HOMEDIR)
+
 with open(TOKENHOME + "token.txt", "r") as readfile:
     TOKEN = readfile.read().strip()
 
-async def acommand(ctx, argument):
-   await bot.say("Stuff")
+bot = discord.ext.commands.Bot(command_prefix = "your_prefix");
 
-bot = commands.Bot(command_prefix='!')
+@bot.command()
+async def ping(ctx):
+    '''Pong! Get the bot's response time'''
+    em = discord.Embed(color=discord.Color.green())
+    em.title = "Pong!"
+    em.description = f'{bot.latency * 1000} ms'
+    await ctx.send(embed=em)
+
 bot.run(TOKEN)
